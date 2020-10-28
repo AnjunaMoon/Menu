bad39f830d426e055e1b4a14d44c5b2554cd309c
chris <chris@ANJUNA>
2020-10-28 04:19:30 +0100

    Add project files.

---
+++ Services/Interfaces/IMenuService.cs
@@ -0,0 +1,19 @@
+﻿using Menu.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Menu.Services.Interfaces
+{
+    interface IMenuService
+    {
+        /// <summary>
+        /// Adds a subitem to an existing MenuItem
+        /// or adds at top-level item (parentItem == null)
+        /// </summary>
+        /// <param name="parentItem"></param>
+        /// <param name="childItem"></param>
+        void AddItem(MenuItem parentItem, MenuItem childItem);
+    }
+}
---
+++ Services/MenuService.cs
@@ -0,0 +1,28 @@
+﻿using Menu.Models;
+using Menu.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Menu.Services
+{
+    public class MenuService : IMenuService
+    {
+        private string dataPath = 
+            Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", "menu.json");
+        
+        public void AddItem(MenuItem parentItem, MenuItem childItem)
+        {
+            using (StreamReader file = File.OpenText(dataPath))
+            {
+                var menu = JsonConvert.DeserializeObject<List<MenuItem>>(file.ReadToEnd());
+                menu.Where(m=>m.)
+            }
+
+        }
+    }
+}
